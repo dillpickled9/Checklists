543d5041b005ae1eb112d30afd95e5736e32b0ea
dillpickled9 <dillpickled9@gmail.com>
2023-02-03 13:27:48 -0500

    Chapter 14

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Dylan DeSantis on 1/29/23.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    var text = ""
+    var checked = false
+}
